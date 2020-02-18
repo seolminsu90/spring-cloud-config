882c38c99177286565631e439ae5ef4f7e46b4d8
msseol <ms.seol@igsinc.co.kr>
2020-02-18 12:23:55 +0900

    eureka

---
+++ spring-cloud-eureka-client-1/src/main/java/com/eureka/SpringCloudEurekaClient1Application.java
@@ -0,0 +1,25 @@
+package com.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudEurekaClient1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClient1Application.class, args);
+    }
+
+    @RestController
+    class EurekaClientController {
+
+        @GetMapping("/eureka/client")
+        public String eurekaClient() {
+            return "eureka client - 1";
+        }
+    }
+}
